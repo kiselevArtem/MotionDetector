1bd0d7cc17f80c9d76a3adbe9e63d26a894abccc
kiselevArtem <89870838314@yandex.ru>
2020-04-07 15:54:35 +0300

    Добавьте файлы проекта.

---
+++ ObjectMotion.cs
@@ -0,0 +1,42 @@
+﻿using AForge;
+using System;
+using System.Collections.Generic;
+
+
+namespace MotionDetectorSample
+{
+    class Car
+    {
+        /// <summary>
+        /// Центр координат машины
+        /// </summary>
+        public List<Point> Route = new List<Point>();
+        /// <summary>
+        ///Состояние показывающее  Отслеживается ли данная машина
+        /// </summary>
+        public bool SostWork=true ;
+        /// <summary>
+        /// Состояние показывающее отслеживалась ли мишина на последнем кадре
+        /// </summary>
+        public bool SostTime = true;
+        /// <summary>
+        /// Количество кадров на которых машина не отслеживалась
+        /// </summary>
+        public int kolvo = 0;
+
+        public void izmenSost()
+        {
+            if (kolvo > 60)
+                SostWork = false;           
+        }
+
+        public void TimeNotWork()
+        {
+            kolvo++;
+            if (kolvo > 60)
+                SostWork = false;
+        }
+    }
+  
+
+}
---
+++ WorkKvadr.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace MotionDetectorSample
+{
+    /// <summary>
+    ///Класс для удаления лишних квадратов
+    /// </summary>
+    class WorkKvadr
+    {
+        /// <summary>
+        /// Главная функция
+        /// </summary>
+        public Rectangle[] main(Rectangle[] a)
+        {
+            Rectangle[] kvadr = a;
+            for (int i=0;i<kvadr.Length;i++)
+            {
+                for (int j = i+1; j < kvadr.Length; j++)
+                {
+
+                }
+            }
+            return kvadr;
+        }
+        /// <summary>
+        /// Определение пересечений квадратов
+        /// </summary>
+        //public bool TimeNotWork()
+        //{
+           
+        //}
+    }
+}
